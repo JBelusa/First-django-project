aae1a6598653caadd59ef7f9ed3d9c8920c54e81
JBelusa <144610582+JBelusa@users.noreply.github.com>
2024-07-08 22:34:16 +0200

    Another staticfile fix
    fix

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,11 +20,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
-urlpatterns = (
-    [
-        path("admin/", admin.site.urls),
-        path("", include("myapp.urls")),
-    ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("myapp.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
++static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
